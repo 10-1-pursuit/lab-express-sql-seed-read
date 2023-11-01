ae712588c558d651dd3c65d1e2af5b9bf494343e
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-01 14:13:40 -0400

    Added the routes for show and create. Also added validation that name and artist are required. Testing both routes with Postman and they are working correctly.

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const songs = express.Router()
-const {getAllSongs} = require("../queries/song")
+const {getAllSongs, getSong, createSong} = require("../queries/song")
+const {checkName, checkArtist, checkBoolean} = require("../validations/checkSongs")
 
 songs.get("/", async (req, res)=>{
     const allSongs = await getAllSongs()
@@ -9,8 +10,22 @@ songs.get("/", async (req, res)=>{
     } else {
         res.status(500).json({error: "SERVER ERROR"})
     }
-    
 })
 
+songs.get("/:id", async (req, res)=>{
+    const {id} = req.params
+    const oneSong = await getSong(id)
+    if(oneSong){
+        res.status(200).json(oneSong)
+    } else {
+        res.status(404).json({error: "No song found at that id"})
+    }
+})
+
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res)=>{
+    const body = req.body
+    const song = await createSong(body)
+    res.status(200).json(song)
+})
 
 module.exports = songs
