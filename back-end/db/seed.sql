a4b226f2c5b4bf2f2cd285c173cf001594caf849
Natyka Callwood <Natykacallwood@pursuit.org>
2023-10-31 22:55:21 -0400

    Index route complete and working.

---
+++ back-end/db/seed.sql
@@ -0,0 +1,9 @@
+ \c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Shea Butter Baby', 'Ari Lennox', 'Shea Butter Baby', 332, true),
+('Damage', 'H.E.R.', 'Back of My Mind', 347, true),
+('Party', 'Beyonce ft. J Cole', 'Single', 340, true),
+('Man Down', 'Rihanna', 'Loud', 427, true),
+('After Hours', 'The Weeknd', 'After Hours', 601, true),
+('Grenade', 'Bruno Mars', 'Doo Wops & Hooligans', 342, true);
