9aa92fbbd1a1981f121fc9168173b79df20ca8f4
jameel914 <jameelibrahim107@gmail.com>
2023-11-01 18:36:59 -0400

    index, show, create are done

---
+++ back-end/controllers/songController.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const songs = express.Router();
+const { getAllSongs, getSong, createSong } = require("../queries/song.js");
+
+songs.get("/", async (req, res) => {
+  const allSongs = await getAllSongs();
+  if (allSongs[0]) {
+    res.status(200).json(allSongs);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const oneSong = await getSong(id);
+  if (oneSong) {
+    res.status(200).json(oneSong);
+  } else {
+    res.status(404).json({ error: "Not Found" });
+  }
+});
+
+songs.post("/", async (req, res) => {
+  const body = req.body;
+  const song = await createSong(body);
+  res.status(200).json(song);
+});
+
+module.exports = songs;
---
+++ back-end/db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Paradox', 'Lipless', 'Sundance', 'March 12, 2021', true),
+('Silence', 'Marshmello', 'Khalid', 'August 11, 2017', false),
+('Liberate', 'Eric Prydz', 'Opus', 'June 3, 2014', false);
