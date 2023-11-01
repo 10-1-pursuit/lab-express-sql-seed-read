5d41b16263549af22f76bcc43407e4362d397ae9
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-01 00:44:18 -0400

    Resolved PORT and (pg-promise)() issue

--- back-end/app.js
+++ back-end/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const songController = require('./controllers/songController');
 
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use('/songs', songController)
+
+//ROUTES
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Songs App!")
+
+});
 
 
+
+module.exports = app;
---
+++ back-end/db/seed.sql
@@ -0,0 +1,8 @@
+\c songs_dev;
+
+INSERT INTO songs (name, album, time, is_favorite)  VALUES
+('Atwa by System of A Down', 'Toxicity', '2:58', TRUE),
+('Paradise City by Guns N Roses', 'Appetite for Destruction', '6:49', FALSE),
+('The Loneliest by Maneskin',  'Rush!',  '4:07', TRUE),
+ ('Wild World by Cat Stevens',  'Tea for the Tillerman', '3:15', FALSE),
+ ('Next to Me by Imagine Dragons', 'Evolve', '3:50', TRUE);
