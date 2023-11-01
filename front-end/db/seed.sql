a3ef8b6bbf6557ad57ff0d9237ac380d2b7adf24
ShakaPursuit <shakalivngstone@pursuit.org>
2023-11-01 12:17:00 -0400

    fished front end

--- front-end/db/seed.sql
+++ front-end/db/seed.sql
@@ -1,5 +1,9 @@
 \c songs_dev;
 
 INSERT INTO songs VALUES
-('Life is Like a Beach Chair','Jay-z','BluePrint' true),
+('Life is Like a Beach Chair','Jay-z','BluePrint',"3:15" ,true),
+('Beat it', 'Michael Jacson', 'Remember the times',"5:43",true ),
+('Everything is Gonna Be Alright', 'Bob Marley',"JamRock", "4:15",true),
+("Damien", "DMX","Its Dark and Hell is Hot", "3:16", true),
+("Rock the Boat", "Aaliyah", "4;00",true)
 ;
