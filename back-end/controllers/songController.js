897ace1375e5b738e499635ac8aad43c3fd0ba76
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-01 12:37:35 -0400

    Added function sinside songController

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -4,8 +4,32 @@ const { getAllSongs } = require('../queries/song');
 
 songs.get('/', async (req, res) => {
     const allSongs = await getAllSongs();
-    res.json(allSongs);
+    if (allSongs[0]) {
+        res.status(200).json(allSongs)
+
+    } else {
+        res.status(500).json({ error: "server error" });
+    }
 });
 
+songs.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const oneSong = await getSong(id);
+
+    if (oneSong) {
+        res.status(200).json(oneSong)
+
+    } else {
+        res.status(404).json({ error: "Not found" })
+    }
+})
+
+songs.post("/", checkName, checkBoolean, async (req, res) => {
+    const body = req.body;
+    const color = await createColor(body)
+
+    res.json(color)
+
+})
 
 module.exports = songs;
