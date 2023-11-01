14a58c0290e112485b6641faa71ba67e9f47f101
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-11-01 02:46:06 -0400

    index route complete & uses pg-promise to query db

---
+++ back-end/db/seed.sql
@@ -0,0 +1,8 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Rise and Shine', 'J. Cole', 'Cole World: The Sideline Story', '4:34', true),
+('January 28th', 'J. Cole', '2014 Forest Hills Drive', '4:02', true),
+('Paper Planes', 'M.I.A.', 'Kala', '3:25', false),
+('Hot In Herre', 'Nelly', 'Nellyville', '3:48', false),
+('the.climb.back', 'J. Cole', 'The Off-Season', '5:06', true);
