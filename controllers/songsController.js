5d48d2b8055e09ba736a77f7590aaf5852263912
marciafoster <mfoster9tre@gmail.com>
2023-11-02 14:16:51 -0400

    got a single song to show in the browser

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const { getAllSongs, getSong } = require('../queries/song')
 const songs = express.Router()
 
 
-songs.get("/", (req, res) =>{
-    res.json({ status: "good"})
+
+songs.get("/", async (req, res) =>{
+    const allSongs = await getAllSongs();
+    if(allSongs[0]) {
+    res.status(200).json(allSongs)
+} else {
+    res.status(500).json({error: "server error"}
+    )
+}
+})
+
+songs.get('/:id', async (req,res) => {
+    const { id } = req.params
+    const oneSong = await getSong(id)
+    if(oneSong){
+        res.status(200).json(oneSong)
+    } else {
+        res.status(404).json({error: 'Not Found' })
+    }
 })
 
 
---
+++ queries/song.js
@@ -0,0 +1,23 @@
+const db = require('../db/dbConfig')
+
+const getAllSongs = async () => {
+    try {
+        const allSongs = await db.any("SELECT * FROM songs");
+        return allSongs
+    } catch (error) {
+        return error
+    }   
+
+}
+
+const getSong = async (id) => {
+    try {
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id)
+        return oneSong
+    } catch (error) {
+        return error
+    }
+
+}
+
+module.exports = { getAllSongs, getSong }
