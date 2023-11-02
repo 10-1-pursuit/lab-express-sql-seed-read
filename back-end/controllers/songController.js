79e19176da0b2ebee31d7021c05e5719631a4dcf
Jesse Chong <jessechong@pursuit.org>
2023-11-02 12:50:24 -0400

    Finished delete and put route

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong, createSong } = require("../queries/song");
+const { getAllSongs, getSong, createSong, updateSong, deleteSong } = require("../queries/song");
 const { checkArtist, checkBoolean, checkAlbum, checkTime } = require("../validations/checkSongs.js")
 
 // INDEX
@@ -28,5 +28,27 @@ songs.get("/:id", async (req, res) => {
     const song = await createSong(body);
     res.json(song);
   });
+
+  songs.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id);
+    if (deletedSong.id) {
+      res.status(200).json(deletedSong);
+    } else {
+      res.status(404).json("Song not found");
+    }
+  });
+  
+  songs.put("/:id", checkBoolean, checkArtist, checkAlbum, checkTime, async (req, res) => {
+    const { id } = req.params
+    const body = req.body
+    const updatedSong = await updateSong(id, body)
+    if(updatedSong.id){
+      res.status(200).json(updatedSong);
+    } else {
+      res.status(404).json("Song not found");
+    }
+  });
+  
 
 module.exports = songs;
--- back-end/queries/song.js
+++ back-end/queries/song.js
@@ -29,6 +29,30 @@ const getSong = async (id) => {
       return error;
     }
   };
+
+  const deleteSong = async (id) => {
+    try {
+      const deletedSong = await db.one(
+        "DELETE FROM songs WHERE id = $1 RETURNING *",
+        id
+      );
+      return deletedSong;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const updateSong = async (id, song) => {
+    try {
+      const updatedSong = await db.one(
+        "UPDATE songs SET artist=$1, album=$2, time=$3, is_favorite=$4 where id=$5 RETURNING *",
+        [song.artist, song.album, song.time, song.is_favorite, id]
+      );
+      return updatedSong;
+    } catch (error) {
+      return error;
+    }
+  };
   
 
-module.exports = { getAllSongs, getSong, createSong };
+module.exports = { getAllSongs, getSong, createSong, deleteSong, updateSong };
