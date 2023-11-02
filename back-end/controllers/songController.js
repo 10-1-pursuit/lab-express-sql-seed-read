12550175c9a23c17313c9988cf2a42358c418f7b
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-02 13:49:52 -0400

    Added the routes for update - put and delete. Also added validation to the put route. Updated the error handling messages for all routes. Tested in Postman and all routes are working correctly.

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const songs = express.Router()
-const {getAllSongs, getSong, createSong} = require("../queries/song")
+const {getAllSongs, getSong, createSong, deleteSong, updateSong} = require("../queries/song")
 const {checkName, checkArtist, checkBoolean} = require("../validations/checkSongs")
 
 songs.get("/", async (req, res)=>{
@@ -18,7 +18,7 @@ songs.get("/:id", async (req, res)=>{
     if(oneSong){
         res.status(200).json(oneSong)
     } else {
-        res.status(404).json({error: "No song found at that id"})
+        res.status(404).json({error: "NO song found at the provided id!"})
     }
 })
 
@@ -27,5 +27,27 @@ songs.post("/", checkName, checkArtist, checkBoolean, async (req, res)=>{
     const song = await createSong(body)
     res.status(200).json(song)
 })
+
+songs.delete("/:id", async (req, res)=>{
+    const {id} = req.params
+    const deletedSong = await deleteSong(id)
+    if(deletedSong.id){
+        res.status(200).json(deletedSong)
+    } else {
+        res.status(404).json({error: "Song NOT deleted!"})
+    }
+})
+
+songs.put("/:id", checkName, checkArtist, checkBoolean, async (req, res)=>{
+    const {id} = req.params
+    const body = req.body
+    const updatedSong = await updateSong(id, body)
+    if(updatedSong.id){
+        res.status(200).json(updatedSong)
+    } else {
+        res.status(404).json({error: "Song NOT updated!"})
+    }
+})
+
 
 module.exports = songs
