20e18b22979ae1f56321293683f4a65c7e6988f4
Christine Marchese <chm978@gmail.com>
2023-11-02 15:03:18 -0400

    crud

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getOneSong, newSong } = require("../queries/song.js")
+const { getAllSongs, getOneSong, newSong, deleteSong, updateSong } = require("../queries/song.js")
 const { checkName, checkArtist, checkBoolean } = require("../validations/checkSongs.js")
 
 
@@ -28,11 +28,34 @@ songs.get("/:id", async (req, res) => {
 });
 
 //Post
-songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
+songs.post("/", checkName, checkBoolean, async (req, res) => {
     const body = req.body;
     const song = await newSong(body);
     res.status(200).json(song);
 });
+
+// Delete
+songs.delete('/:id', async ( req, res) => {
+     const { id } = req.params
+     const deletedSong = await deleteSong(id)
+       if(deletedSong.id) {
+         res.status(200).json(deletedSong)
+       } else {
+          res.status(404).json({ error: "Song Not Found" })
+       }
+})
+
+// PUT
+songs.put("/:id", checkName, checkArtist, checkBoolean, async (req, res) => {
+    const {id} = req.params
+    const body = req.body
+    const updatedSong = await updateSong(id, body)
+     if(updatedSong.id) {
+        res.status(200).json(updatedSong)
+     } else {
+       res.status(400).json({ error: "Song Not Found"})
+     }
+})
 
 
 
--- back-end/queries/song.js
+++ back-end/queries/song.js
@@ -31,5 +31,28 @@ const newSong = async (song) => {
     }
 }  
 
+const deleteSong = async (id) => {
+     try{
 
-module.exports = { getAllSongs, getOneSong, newSong};
+       const deletedSong = await db.one("DELETE FROM songs WHERE id=$1 RETURNING * ", id)
+        return deletedSong
+     } catch (error) {
+        return error
+     }
+}
+
+
+const updateSong = async (id, song) => {
+    try {
+      const updatedSong = await db.one(
+        "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURNING *",
+        [song.name, song.artist, song.album, song.time, song.is_favorite, id]
+      );
+      return updatedSong;
+    } catch (error) {
+      return error;
+    }
+  };
+
+
+module.exports = { getAllSongs, getOneSong, newSong, deleteSong, updateSong};
