ab6eeeeed087d08b6a09555d3a5eede91d7b0182
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-02 12:38:03 -0400

    created routes for delete and update

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { getAllTunes, getTune, createTune } = require("../queries/tuner");
+const {
+  getAllTunes,
+  getTune,
+  createTune,
+  deleteTune,
+  updateTune,
+} = require("../queries/tuner");
 const songs = express.Router();
-const {checkName, checkArtist,checkBoolean} = require("../validations/checkTunes")
+const {
+  checkName,
+  checkArtist,
+  checkBoolean,
+} = require("../validations/checkTunes");
 //get all tunes
 
 songs.get("/", async (req, res) => {
@@ -9,19 +19,40 @@ songs.get("/", async (req, res) => {
   res.json(allTunes);
 });
 
-songs.get("/:id", async(req,res)=>{
-  const {id} = req.params;
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
   const song = await getTune(id);
-  if(song){
-    res.status(200).json(song)
-  }else{
-    res.status(404).json({error: "Song not found"})
+  if (song) {
+    res.status(200).json(song);
+  } else {
+    res.status(404).json({ error: "Song not found" });
   }
 });
 
-songs.post("/",checkName, checkArtist, checkBoolean, async (req,res)=>{
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
   const song = await createTune(req.body);
   res.status(200).json(song);
-})
+});
+
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteTune(id);
+  if (deletedSong.id) {
+    res.status(200).json(deletedSong);
+  } else {
+    res.status(404).json({ message: "Tune not found" });
+  }
+});
+
+songs.put("/:id", checkName, checkArtist, checkBoolean, async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const updatedSong = await updateTune(id, body);
+  if (updatedSong.id) {
+    res.status(200).json(updatedSong);
+  } else {
+    res.status(404).json({ error: "Tune Not Found" });
+  }
+});
 
 module.exports = songs;
--- back-end/queries/tuner.js
+++ back-end/queries/tuner.js
@@ -9,18 +9,52 @@ const getAllTunes = async () => {
   }
 };
 
-const getTune = async(id)=>{
+const getTune = async (id) => {
   try {
-    const oneSong = await db.one("SELECT * FROM tuners WHERE ID=$1", id)
-    return oneSong
+    const oneSong = await db.one("SELECT * FROM tuners WHERE ID=$1", id);
+    return oneSong;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
-const createTune = async(song)=>{
-  const newSong = await db.one("INSERT INTO tuners (name, artist, album ,time, is_favorite) VALUES ($1,$2,$3,$4,$5) RETURNING *",[song.name,song.artist,song.album,song.title,song.time,song.is_favorite]);
-  return newSong
-}
+const createTune = async (song) => {
+  const newSong = await db.one(
+    "INSERT INTO tuners (name, artist, album ,time, is_favorite) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+    [
+      song.name,
+      song.artist,
+      song.album,
+      song.title,
+      song.time,
+      song.is_favorite,
+    ]
+  );
+  return newSong;
+};
 
-module.exports = { getAllTunes, getTune, createTune };
+const deleteTune = async (id) => {
+  try {
+    const deletedSong = await db.one(
+      "DELETE FROM tuners WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateTune = async (id, song) => {
+  try {
+    const updatedSong = await db.one(
+      "UPDATE tuners SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 Returning *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite, id]
+    );
+    return updatedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllTunes, getTune, createTune, deleteTune, updateTune };
