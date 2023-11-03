44cf6b7c9cc6030f896d7c45055dc661f1079a0f
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-03 06:13:53 -0400

    congcontroller

---
+++ back-end/controllers/songController.js
@@ -0,0 +1,17 @@
+// controllers/songController.js
+const express = require("express");
+const songs = express.Router();
+const { getAllsongs } = require("../queries/song");
+
+// INDEX
+songs.get("/", async (req, res) => {
+  const allSongs = await getAllSongs();
+  if (allSongs[0]) {
+    res.status(200).json(allSongs);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+
+module.exports = songs;
