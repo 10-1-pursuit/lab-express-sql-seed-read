7eb98410e94e05783d3ae188ff33427894529d38
Jesse Chong <jessechong@pursuit.org>
2023-11-04 13:39:03 -0400

    First commit and homepage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,29 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+// PAGES
+import Home from './Pages/Home';
+
 function App() {
   return (
-    <div className="">
-      <h1>Tuner App</h1>
+<Router>
+    <div className="app">
+<Navbar className="d-flex justify-content-between" bg="warning" >
+          <Nav>
+            <Nav.Link href="/songs">Songs</Nav.Link>
+          </Nav>
+          <Navbar.Brand>Tuner App</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/songs/new">New Song</Nav.Link>
+          </Nav>
+        </Navbar>
+<Routes>
+  <Route path='/' element={<Home />} />
+
+</Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ front-end/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className="Home">
+        <h2>✋ Hello 🤚</h2>
+        <h3>🎹 Welcome to the Tuner App 🎸</h3>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
