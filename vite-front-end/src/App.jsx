78f20fc658f6b2662e0f84b2ce2050a778561fbb
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-11-06 02:28:36 -0500

    songs:index with buttons & delete functionality

--- vite-front-end/src/App.jsx
+++ vite-front-end/src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar.jsx'
 import Home from './pages/Home.jsx'
 import Index from './pages/Index.jsx'
 import New from './pages/New.jsx'
+import Show from './pages/Show.jsx'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path='/' element={<Home />}/>
             <Route path='/songs' element={<Index />}/>
             <Route path='/songs/new' element={<New />}/>
+            <Route path='/songs/:id' element={<Show />}/>
           </Routes>
         </main>
       </Router>
---
+++ vite-front-end/src/pages/Show.jsx
@@ -0,0 +1,10 @@
+import SongDetails from '../components/SongDetails.jsx';
+
+export default function Show() {
+    return (
+        <div>
+            <h2>Show</h2>
+            <SongDetails />
+        </div>
+    )
+}
