4d95f5c414d681526f53cd2ef1b20ab2521b1953
Jesse Chong <jessechong@pursuit.org>
2023-11-10 17:55:40 -0500

    Added another table to seed and schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -2,11 +2,19 @@ DROP DATABASE IF EXISTS tuner;
 CREATE DATABASE tuner;
 \c tuner;
 
+CREATE TABLE playlists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE songs (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   artist TEXT NOT NULL,
   album TEXT NOT NULL,
-  time TEXT,
-  is_favorite BOOLEAN
+  time INT,
+  is_favorite BOOLEAN,
+  playlist_id INTEGER REFERENCES playlists(id)
+ ON DELETE CASCADE
 );
